ec4cf6ab57a436b84e38c03b476980c7ec59efcc
alex-kurkov <alexkourkov@yandex.ru>
2021-05-22 14:56:02 +0500

    fix: rewrite App.js to functional + redux hooks

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,87 +1,66 @@
 import agent from '../agent';
-import React from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { APP_LOAD, REDIRECT } from '../constants/actionTypes';
 import { Route, Switch } from 'react-router-dom';
 import Header from './Header';
-import Article from '../components/Article';
-import Editor from '../components/Editor';
-import Home from '../components/Home';
-import Login from '../components/Login';
-import Profile from '../components/Profile';
-import ProfileFavorites from '../components/ProfileFavorites';
-import Register from '../components/Register';
-import Settings from '../components/Settings';
-import { store } from '../store';
-// import { push } from 'react-router-redux';
-import { push } from 'connected-react-router';
+import Article from './Article';
+import Editor from './Editor';
+import Home from './Home';
+import Login from './Login';
+import Profile from './Profile';
+import ProfileFavorites from './ProfileFavorites';
+import Register from './Register';
+import Settings from './Settings';
 
-const mapStateToProps = state => {
-  return {
-    appLoaded: state.common.appLoaded,
-    appName: state.common.appName,
-    currentUser: state.common.currentUser,
-    redirectTo: state.common.redirectTo
-  }};
-
-const mapDispatchToProps = dispatch => ({
-  onLoad: (payload, token) =>
-    dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
-  onRedirect: () =>
-    dispatch({ type: REDIRECT })
-});
-
-class App extends React.Component {
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.redirectTo) {
-      // this.context.router.replace(nextProps.redirectTo);
-      store.dispatch(push(nextProps.redirectTo));
-      this.props.onRedirect();
-    }
-  }
-
-  componentWillMount() {
+  const App = () => {
+    const dispatch = useDispatch();
+    const { appLoaded, appName, currentUser, redirectTo } = useSelector(state => state.common)
+    
+/*     useEffect(() => {
+      dispatch({ type: REDIRECT })
+    }, [ redirectTo ])
+ */
+    useEffect(() => {
+      const token = window.localStorage.getItem('jwt');
+      if (token) {
+        agent.setToken(token);
+      }
+      dispatch({ type: APP_LOAD, payload: token ? agent.Auth.current() : null, token, skipTracking: true })
+    }, [])
+  /* componentWillMount() {
     const token = window.localStorage.getItem('jwt');
     if (token) {
       agent.setToken(token);
     }
 
     this.props.onLoad(token ? agent.Auth.current() : null, token);
-  }
+  } */
 
-  render() {
-    if (this.props.appLoaded) {
-      return (
-        <div>
+    return appLoaded 
+      ? (
+        <>
           <Header
-            appName={this.props.appName}
-            currentUser={this.props.currentUser} />
-            <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/editor/:slug" component={Editor} />
-            <Route path="/editor" component={Editor} />
-            <Route path="/article/:id" component={Article} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/@:username/favorites" component={ProfileFavorites} />
-            <Route path="/@:username" component={Profile} />
-            </Switch>
-        </div>
-      );
-    }
-    return (
-      <div>
-        <Header
-          appName={this.props.appName}
-          currentUser={this.props.currentUser} />
-      </div>
-    );
-  }
+            appName={appName}
+            currentUser={currentUser} />
+          <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/editor/:slug" component={Editor} />
+          <Route path="/editor" component={Editor} />
+          <Route path="/article/:id" component={Article} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/@:username/favorites" component={ProfileFavorites} />
+          <Route path="/@:username" component={Profile} />
+          </Switch>
+        </>
+      )
+      : (
+          <Header
+            appName={appName}
+            currentUser={currentUser} />
+      )
 }
 
-// App.contextTypes = {
-//   router: PropTypes.object.isRequired
-// };
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
