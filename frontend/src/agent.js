b390b6c89c4e6669d3625c9f67364cefc9c1efdd
Mikhail Lisus <lisusmikhail@gmail.com>
2021-05-29 23:15:06 -0400

    design system v1.0

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -3,13 +3,13 @@ import _superagent from 'superagent'
 
 const superagent = superagentPromise(_superagent, global.Promise)
 
-// const API_ROOT = process.env.API_HOST
-//   ? process.env.API_HOST
-//   : 'https://api.pickledbananas.com/api'
-
 const API_ROOT = process.env.API_HOST
   ? process.env.API_HOST
-  : 'http://localhost:3000/api'
+  : 'https://api.pickledbananas.com/api'
+
+// const API_ROOT = process.env.API_HOST
+//   ? process.env.API_HOST
+//   : 'http://localhost:3000/api'
 
 const encode = encodeURIComponent
 const responseBody = (res) => res.body
